3ad7696a110035f4d5296d2f5d2af8fc8202ffdc
majiabao <majiabaoIT@163.com>
2018-02-28 15:10:01 +0800

    VC

---
+++ common-util/src/main/java/ClassFieldNameTransform.java
@@ -0,0 +1,22 @@
+import com.alibaba.dubbo.common.utils.StringUtils;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author majiabao on 2018/2/28.
+ */
+public class ClassFieldNameTransform {
+    public static void main(String[] args) {
+        transfrom(SampleItemDongDBView.class);
+    }
+
+    public static void transfrom(Class<?> source) {
+        //todo 以后完善吧，先直接将属性打印
+        //最好优化为直接生成一个类
+        Field[] fields = source.getDeclaredFields();
+        String modifier = "private ";
+        for (Field field : fields) {
+            System.out.println(modifier + field.getType().getSimpleName() + " " + StringUtils.camelToSplitName(field.getName(), "_") + ";");
+        }
+    }
+}
