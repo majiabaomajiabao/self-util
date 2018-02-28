913a7c57ac1d8e596d163be22ff78e97533f4a9b
majiabao <majiabaoIT@163.com>
2018-02-28 16:36:33 +0800

    C

--- common-util/src/main/java/ClassFieldNameTransform.java
+++ common-util/src/main/java/ClassFieldNameTransform.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Field;
  */
 public class ClassFieldNameTransform {
     public static void main(String[] args) {
-        transfrom(SampleItemDongDBView.class);
+        transfrom0(SampleItemDongDBView.class);
     }
 
     public static void transfrom(Class<?> source) {
@@ -17,6 +17,15 @@ public class ClassFieldNameTransform {
         String modifier = "private ";
         for (Field field : fields) {
             System.out.println(modifier + field.getType().getSimpleName() + " " + StringUtils.camelToSplitName(field.getName(), "_") + ";");
+        }
+    }
+
+    public static void transfrom0(Class<?> source) {
+        //todo 以后完善吧，先直接将属性打印
+        //最好优化为直接生成一个类
+        Field[] fields = source.getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(field.getName()+",");
         }
     }
 }
